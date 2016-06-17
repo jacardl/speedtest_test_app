d0b45ac72e2e46cb0f5bd357e0780d8f10ffec6d
Jac <u_jar@sina.com>
2016-06-17 17:28:33 +0800

    1. call ookla app

--- app/src/main/java/com/testookla/TestOokla.java
+++ app/src/main/java/com/testookla/TestOokla.java
@@ -1,5 +1,6 @@
 package com.testookla;
 
+import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
 
 import com.robotium.solo.Solo;
@@ -7,7 +8,7 @@ import com.robotium.solo.Solo;
 @SuppressWarnings("unchecked")
 public class TestOokla extends ActivityInstrumentationTestCase2 {
 
-//	private static final String LAUNCHER_ACTIVITY_FULL_CLASSNAME = "com.peanutswifi.MainActivity";
+//	public static final String ROOT = Environment.getExternalStorageDirectory().getPath() + "/test/";
 	private static final String LAUNCHER_ACTIVITY_FULL_CLASSNAME =
         "com.ookla.speedtest.softfacade.MainActivity";
 	private static Class launcherActivityClass;
@@ -30,14 +31,31 @@ public class TestOokla extends ActivityInstrumentationTestCase2 {
 	@Override
 	protected void setUp() throws Exception {
 //		solo = new Solo(getInstrumentation(), getActivity());
+//		solve getActivity() suspend AUT bug by this url below
+//		http://stackoverflow.com/questions/20860832/why-does-getactivity-block-during-junit-test-when-custom-imageview-calls-start?answertab=oldest#tab-top
         solo = new Solo(getInstrumentation());
+		Intent intent = new Intent(getInstrumentation().getTargetContext(), launcherActivityClass);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		getInstrumentation().getTargetContext().startActivity(intent);
+
 	}
 
-	public void test_ookla() {
-        solo.sleep(5000);
-		solo.clickOnText("Begin Test");
-//        solo.waitForText("SHARE", 0, 60000);
-        solo.sleep(60000);
+	public void test_ookla_speedtest() throws Exception{
+
+//		String fileName = "routerTest";
+//		String content = "this is a test";
+//		File file = new File(ROOT + fileName);
+//		try {
+//			FileUtils.writeStringToFile(file, content);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		solo.sleep(5*1000);
+//		solo.clickOnText("Begin Test");
+//		solo.sleep(20*1000);
+//		solo.clickOnText("No thanks");
+//		solo.sleep(5*1000);
+		solo.takeScreenshot("12345");
 		// Verify that resultant of 10 x 20
 //		assertTrue("xxxxxxxxxxxxxxxxxxx", expect);
 	}
